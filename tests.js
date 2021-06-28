8953a1094bce1c566a6236872f916a9d9ad99474
Michael Coyle <michael.david.coyle4@gmail.com>
2021-06-27 23:15:28 -0500

    Added first test. Test failing.

---
+++ tests.js
@@ -0,0 +1,6 @@
+// Unit tests for helloWorld
+describe('helloWorld', function() {
+    it('should be a defined function', function() {
+        expect(typeof helloWorld).toBe('function');
+    });
+});
