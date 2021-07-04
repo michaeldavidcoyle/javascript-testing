3ac6bb3d016031dfc95a760c9e85620cf157760d
Michael Coyle <michael.david.coyle4@gmail.com>
2021-07-04 00:43:14 -0500

    Refactored

--- code.js
+++ code.js
@@ -5,8 +5,11 @@ function helloWorld() {
 // Create a function that imitates String.charAt, returns
 // the character at the input position
 function stringCharAt(string, position) {
-    if (typeof string !== 'string') return undefined;
-    if (!Number.isInteger(position)) return undefined;
-    if (position < 0 || position >= string.length) return '';
-    return string[position];
+    if (typeof string === 'string' && Number.isInteger(position)) {
+        if (position >= 0 && position < string.length) {
+            return string[position];
+        }
+
+        return '';
+    }
 }
